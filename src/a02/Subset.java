5304e6d36da42bbb9646a72a9e9830a007a56ceb
Conner Jensen <connerjensen@Conners-Mac-mini.local>
2021-09-26 09:08:36 -0600

    hi

--- src/a02/Subset.java
+++ src/a02/Subset.java
@@ -7,17 +7,14 @@ import edu.princeton.cs.algs4.StdIn;
 public class Subset {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int k = input.nextInt();
-		
-		System.out.println(k);
+		int k = 5;
+		String[] input = {"hi", "there", "how", "is", "it", "going"};
 		
 		RandomizedQueue<String> rq = new RandomizedQueue<String>();
 		
 		
-		while(!StdIn.isEmpty()) {
-			String s = StdIn.readString();
-			rq.enqueue(s);
+		for(int i=0; i < input.length; i++) {
+			rq.enqueue(input[i]);
 		}
 		
 		
